c124e23330a7e27cb0cc78f267da8b06fab1c5b7
Amos Godwin <amosgodwin500@gmail.com>
2024-01-10 12:05:53 +0100

    added pagination support

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@
 
 abstract class ApiConfig {
 
-  static  String baseUrl = 'https://api.giphy.com/v1/gifs/';
-  static  String trendingGifs ({required String apiKey,}) => '$baseUrl/trending?api_key=$apiKey&limit=25&offset=0&rating=g&bundle=messaging_non_clips';
+  static  String baseUrl = 'https://api.giphy.com/v1/gifs';
+  static  String trendingGifs ({required String apiKey,required int offset}) => '$baseUrl/trending?api_key=$apiKey&limit=25&offset=$offset&rating=g&bundle=messaging_non_clips';
 
 }
